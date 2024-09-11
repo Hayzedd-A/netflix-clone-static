b2695eb4313bce5e4ea429c0570d195750b353b2
Hayzedd-A <adebayoazeez37@yahoo.com>
2024-09-11 03:31:50 +0100

    update, add favicon to html, remove input functionality from script, its done with css, fix body over wide, used overflow

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-// $("body").click(() => {
-//   console.log("event captured on body");
-//   if ($(".form input").val().length) {
-//     console.log("if run");
-//     $(".form label").addClass("content");
-//   } else {
-//     console.log("else run");
-//     $(".form label").removeClass("content");
-//   }
-// });
-
 $("document").ready(() => {
   console.log("DOM fully loaded and parsed");
   for (let i = 1; i <= 10; i++) {
